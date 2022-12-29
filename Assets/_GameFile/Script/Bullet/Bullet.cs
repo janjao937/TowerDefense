73128a22f51af50cfb8477c20643ea9ee82bef3a
janjao937 <janjao937@gmail.com>
2022-12-29 07:47:27 +0700

    cmmit

--- Assets/_GameFile/Script/Bullet/Bullet.cs
+++ Assets/_GameFile/Script/Bullet/Bullet.cs
@@ -30,7 +30,7 @@ public abstract class Bullet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) 
     {
-        if(other?.gameObject?.GetComponent<Bullet>()) return;
+        if(!other?.gameObject?.GetComponent<Enemy>()) return;
         
         HitObject();
     }
